940280509df4107fd8dcf4b7ac47acd4a2e906b1
philip <p.vendil@cgi.com>
2023-03-23 11:20:39 +0100

    EIDDSS-733 prepare for mirroring support-lib and java-configuration to Github.

--- src/main/java/se/signatureservice/configuration/common/InternalServerException.java
+++ src/main/java/se/signatureservice/configuration/common/InternalServerException.java
@@ -1,15 +1,16 @@
 /************************************************************************
-*                                                                       *
-*  Certificate Service - Common          ,                               *
-*                                                                       *
-*  This software is free software; you can redistribute it and/or       *
-*  modify it under the terms of the GNU Affero General Public License   *
-*  License as published by the Free Software Foundation; either         *
-*  version 3   of the License, or any later version.                    *
-*                                                                       *
-*  See terms of license at gnu.org.                                     *
-*                                                                       *
-*************************************************************************/
+ *                                                                       *
+ *  Signservice Java Configuration                                       *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public License   *
+ *  (LGPL-3.0-or-later)                                                  *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3 of the License, or any later version.                      *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package se.signatureservice.configuration.common;
 
 /**
--- src/main/java/se/signatureservice/configuration/common/cache/CacheProvider.java
+++ src/main/java/se/signatureservice/configuration/common/cache/CacheProvider.java
@@ -1,3 +1,16 @@
+/************************************************************************
+ *                                                                       *
+ *  Signservice Java Configuration                                       *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public License   *
+ *  (LGPL-3.0-or-later)                                                  *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3 of the License, or any later version.                      *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package se.signatureservice.configuration.common.cache;
 
 import se.signatureservice.configuration.common.InternalErrorException;
