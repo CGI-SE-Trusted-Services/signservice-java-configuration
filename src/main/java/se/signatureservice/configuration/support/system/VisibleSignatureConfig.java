511806f9afa1ca2bc8b021cefb7e479d84bb2bbe
filipwessman <filip.wessman@cgi.com>
2025-05-08 20:32:33 +0200

    license header fix for signservice-java-configuration, -support-lib and transactionsigning

--- src/main/java/se/signatureservice/configuration/support/system/VisibleSignatureConfig.java
+++ src/main/java/se/signatureservice/configuration/support/system/VisibleSignatureConfig.java
@@ -1,10 +1,9 @@
 /************************************************************************
  *                                                                       *
- *  Signservice Java Configuration                                       *
+ *  Signature Service - Java Configuration Library                       *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
  *  modify it under the terms of the GNU Lesser General Public License   *
- *  (LGPL-3.0-or-later)                                                  *
  *  License as published by the Free Software Foundation; either         *
  *  version 3 of the License, or any later version.                      *
  *                                                                       *
